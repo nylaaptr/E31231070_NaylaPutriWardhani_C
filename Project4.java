b50f119840174eb10cab5fad77526f47b8ec7be5
nylaaptr <naylawardhani488@gmail.com>
2024-01-29 14:50:51 +0700

    Create Project4.java

---
+++ Project4.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+public class Project4 {
+    public static void main(String[] args) {
+        System.out.println("Sebelum for"); 
+        for(int a=0;a<10;a++) {
+        if(a==4) 
+        break; 
+        System.out.println("Nilai a : "+ a);
+}
+        System.out.println("Setelah for");
+    }
+}
